451466f03abf74354bd3ed531cb4dd70a60d76cf
RajCutinha <rajath.cutinha@protonmail.com>
2022-02-23 01:08:46 +0100

    added diashow function frontpage

--- app.js
+++ app.js
@@ -4,3 +4,58 @@ const navigationBar = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
   navigationBar.classList.toggle("active");
 });
+
+const diashowImgs = document.querySelector(".diashow").querySelectorAll("img");
+const diashowPageination = document
+  .querySelector(".diashow")
+  .querySelectorAll("ul li");
+
+let currentDiashowIndex = 0;
+
+function diashow() {
+  currentDiashowIndex++;
+  if (currentDiashowIndex === diashowImgs.length) {
+    currentDiashowIndex = 0;
+    diashowImgs.forEach((img) => {
+      img.style.transition = "unset";
+      img.style.transform = "translateX(-" + currentDiashowIndex * 100 + "%)";
+    });
+    diashowPageination[currentDiashowIndex].classList.add("active");
+    return;
+  }
+
+  if (!(currentDiashowIndex > diashowPageination.length - 1)) {
+    diashowPageination[currentDiashowIndex].classList.add("active");
+    diashowPageination[
+      currentDiashowIndex
+    ].previousElementSibling.classList.remove("active");
+  } else {
+    diashowPageination[currentDiashowIndex - 1].classList.remove("active");
+    diashowPageination[currentDiashowIndex - currentDiashowIndex].classList.add(
+      "active"
+    );
+  }
+
+  diashowImgs.forEach((img) => {
+    img.style.transition = "all 1s ease-in-out";
+    img.style.transform = "translateX(-" + currentDiashowIndex * 100 + "%)";
+  });
+}
+
+let diashowInterval = window.setInterval(diashow, 5000);
+
+diashowPageination.forEach((index) => {
+  index.addEventListener("click", (e) => {
+    currentDiashowIndex = e.target.dataset.value;
+    diashowImgs.forEach((img) => {
+      img.style.transition = "all 1s ease-in-out";
+      img.style.transform = "translateX(-" + currentDiashowIndex * 100 + "%)";
+    });
+    diashowPageination.forEach((pageNation) =>
+      pageNation.classList.remove("active")
+    );
+    diashowPageination[currentDiashowIndex].classList.add("active");
+    window.clearInterval(diashowInterval);
+    diashowInterval = window.setInterval(diashow, 5000);
+  });
+});
