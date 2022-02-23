4467b7c0fa30ec250290a6624f892d7a597119c2
RajCutinha <rajath.cutinha@protonmail.com>
2022-02-23 01:29:19 +0100

    added hamburger animation + box shadow to nav

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const menuBtn = document.querySelector(".menubar-mobile div");
 const navigationBar = document.querySelector("nav");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", (e) => {
+  e.target.classList.toggle("active");
   navigationBar.classList.toggle("active");
 });
 
