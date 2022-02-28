fc6ffb44f7f89b0544b67f537b93729b784c3d08
RajCutinha <rajath.cutinha@protonmail.com>
2022-02-28 16:04:37 +0100

    update folder structure assets

--- ani/speisekarte.js
+++ ani/speisekarte.js
@@ -1,4 +1,4 @@
-import gsap, { Power4, Back } from "gsap";
+import gsap, { Power4 } from "gsap";
 
 const gridTl = gsap.timeline({ defaults: { duration: 0.3 } });
 const gridChilds = document.querySelectorAll(".karte-grid a");
